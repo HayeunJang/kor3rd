ec11eaa8f06a2d7b8ffcde245df7be42d2bc8489
HayeunJang <hyjang@skku.edu>
2026-01-27 01:56:55 +0900

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,3 +1,3 @@
 // js/config.js
-export const API_URL = "https://script.google.com/macros/s/AKfycby_3IVJ4gx82_ZWKa2Tbu-VnbJbz7179IEmsxwmCJf0AzXLMoarU5sLuokoqc6Ig4doGg/exec";
+export const API_URL = "https://script.google.com/macros/s/AKfycbzlt2igjZ-ibcGGkg4Qepwd-1vsbchMKsnn8eKWysNsRSh_ifF7x5uSBpvFv1ZdhA5Dpg/exec";
 export const SECRET  = "priming_2026_korean_nucleus_preparation_SECRET";
